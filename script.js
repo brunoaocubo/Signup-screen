0fe0e006b09ddb8e4c89ac0b9ce76eefc470dfe5
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-01-14 17:45:26 -0300

    Feedback de Validação - 75%
    O formulário de criação da conta está na fase onde preciso passar todos os feedbacks necessários de acordo com o preview active states. Ainda não está concluido.

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let validationForms = function(){
         {
             inputs[i].classList.add('error')   
             inputs[i].placeholder = ''
+            inputs[i].style.border = '2px solid var(--Red400)'
         }
         else if(inputs[i].classList.contains('error') && inputs[i].value != '')
         {
@@ -17,3 +18,15 @@ let validationForms = function(){
 
 let submit = document.querySelector('#submit')
 submit.addEventListener('click', validationForms)
+
+let placeholders = [['firstName', 'First Name'], ['lastName', 'Last Name'], ['email', 'Email Address'], ['password', 'Password']]
+
+for(let i = 0; i < inputs.length; i++){
+    inputs[i].addEventListener('mouseout', function(){
+        for(let p = 0; p < placeholders.length; p++){
+            if(inputs[i].id == placeholders[p][0])
+            inputs[i].placeholder = placeholders[p][1]
+            inputs[i].style.textAlign = 'center' 
+        }
+    })
+}
