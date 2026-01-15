3c1adf5136955f88dd7b9fb50edf8f4f7778ec05
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-01-15 11:59:50 -0300

    Feedback de Validação - 90%
    Consertado os problemas de lógica ao clicar, inserir um valor e aparecer o feedback, agora falta apenas dizer os motivos descritivo para o erro que aparece.

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 let inputs = document.querySelectorAll('.inputSignup')
+let submit = document.querySelector('#submit')
+let placeholders = [['firstName', 'First Name'], ['lastName', 'Last Name'], ['email', 'Email Address'], ['password', 'Password']]
 
 let validationForms = function(){
     for(let i = 0; i < inputs.length; i++)
     {
-        if(!inputs[i].classList.contains('error') && inputs[i].value == '')
+        if(!inputs[i].classList.contains('error') && inputs[i].value === '')
         {
-            inputs[i].classList.add('error')   
-            inputs[i].placeholder = ''
-            inputs[i].style.border = '2px solid var(--Red400)'
-        }
-        else if(inputs[i].classList.contains('error') && inputs[i].value != '')
-        {
-            inputs[i].classList.remove('error')
+            inputs[i].classList.add('error');
+
+            if(inputs[i].id == 'email'){
+                inputs[i].placeholder = 'email@example/com'
+            }
+            else{
+                inputs[i].placeholder = '';
+            }
         }
     }
 }
 
-let submit = document.querySelector('#submit')
-submit.addEventListener('click', validationForms)
-
-let placeholders = [['firstName', 'First Name'], ['lastName', 'Last Name'], ['email', 'Email Address'], ['password', 'Password']]
-
 for(let i = 0; i < inputs.length; i++){
-    inputs[i].addEventListener('mouseout', function(){
-        for(let p = 0; p < placeholders.length; p++){
-            if(inputs[i].id == placeholders[p][0])
-            inputs[i].placeholder = placeholders[p][1]
-            inputs[i].style.textAlign = 'center' 
+    inputs[i].addEventListener('mouseout', function()
+    {
+        for(let p = 0; p < placeholders.length; p++)
+        {
+            if(inputs[i].id === placeholders[p][0] && !inputs[i].classList.contains('error'))
+            {
+                inputs[i].placeholder = placeholders[p][1]
+            }
+        }
+    })
+
+    inputs[i].addEventListener('keydown', function()
+    {
+        if(inputs[i].classList.contains('error'))
+        {
+            inputs[i].classList.remove('error')
         }
     })
 }
+
+submit.addEventListener('click', validationForms)
